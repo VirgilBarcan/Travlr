29e9c4368c4f80fef41e4485a02e21182adf284b
berendeanicolae <berendeanicolae@gmail.com>
2015-05-31 00:15:52 +0300

    add procedures header

--- app/assets/sql/package.sql
+++ app/assets/sql/package.sql
@@ -6,6 +6,33 @@ IS
     p_password IN VARCHAR2)
   RETURN INTEGER;
   
+    procedure addAirport(airport_id AIRPORT.airport_id%TYPE,
+                        airport_name AIRPORT.airport_name%TYPE,
+                        city AIRPORT.city%TYPE,
+                        country AIRPORT.country%TYPE,
+                        iata_faa_code AIRPORT.iata_faa_code%TYPE,
+                        icao_code AIRPORT.icao_code%TYPE,
+                        latitude AIRPORT.latitude%TYPE,
+                        longitude AIRPORT.longitude%TYPE,
+                        altitude AIRPORT.altitude%TYPE,
+                        timezone AIRPORT.timezone%TYPE,
+                        dst AIRPORT.dst%TYPE,
+                        tzone_db_time AIRPORT.tzone_db_time%TYPE);
+    procedure addAirline(airline_id AIRLINE.airline_id%TYPE,
+                        airline_name AIRLINE.airline_name%TYPE,
+                        airline_alias AIRLINE.airline_alias%TYPE,
+                        iata_code AIRLINE.iata_code%TYPE,
+                        icao_code AIRLINE.icao_code%TYPE,
+                        callsign AIRLINE.callsign%TYPE,
+                        country AIRLINE.country%TYPE,
+                        active AIRLINE.active%TYPE);
+    procedure addFlights(flight_id FLIGHT.flight_id%TYPE,
+                        airline_id FLIGHT.airline_id%TYPE,
+                        src_airport FLIGHT.src_airport%TYPE,
+                        dst_airport FLIGHT.dst_airport%TYPE,
+                        codeshare FLIGHT.codeshare%TYPE,
+                        stops FLIGHT.stops%TYPE,
+                        equipment FLIGHT.equipment%TYPE);
   
 END TRAVLR;
 /
@@ -33,4 +60,41 @@ IS
       
       RETURN v_return;
     END ADD_USER;
+    
+    procedure addAirport(airport_id AIRPORT.airport_id%TYPE,
+                        airport_name AIRPORT.airport_name%TYPE,
+                        city AIRPORT.city%TYPE,
+                        country AIRPORT.country%TYPE,
+                        iata_faa_code AIRPORT.iata_faa_code%TYPE,
+                        icao_code AIRPORT.icao_code%TYPE,
+                        latitude AIRPORT.latitude%TYPE,
+                        longitude AIRPORT.longitude%TYPE,
+                        altitude AIRPORT.altitude%TYPE,
+                        timezone AIRPORT.timezone%TYPE,
+                        dst AIRPORT.dst%TYPE,
+                        tzone_db_time AIRPORT.tzone_db_time%TYPE) as
+    begin
+        null;
+    end;
+    procedure addAirline(airline_id AIRLINE.airline_id%TYPE,
+                        airline_name AIRLINE.airline_name%TYPE,
+                        airline_alias AIRLINE.airline_alias%TYPE,
+                        iata_code AIRLINE.iata_code%TYPE,
+                        icao_code AIRLINE.icao_code%TYPE,
+                        callsign AIRLINE.callsign%TYPE,
+                        country AIRLINE.country%TYPE,
+                        active AIRLINE.active%TYPE) as
+    begin
+        null;
+    end;
+    procedure addFlights(flight_id FLIGHT.flight_id%TYPE,
+                        airline_id FLIGHT.airline_id%TYPE,
+                        src_airport FLIGHT.src_airport%TYPE,
+                        dst_airport FLIGHT.dst_airport%TYPE,
+                        codeshare FLIGHT.codeshare%TYPE,
+                        stops FLIGHT.stops%TYPE,
+                        equipment FLIGHT.equipment%TYPE) as
+    begin
+        null;
+    end;
 END TRAVLR;
