d3603e80ed0a38be9df5b5b0f271ade401d321d4
berendeanicolae <berendeanicolae@gmail.com>
2015-05-31 20:26:57 +0300

    format strings

--- app/service/Importer.java
+++ app/service/Importer.java
@@ -3,6 +3,8 @@ package service;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -25,17 +27,27 @@ public class Importer {
 		void setDefaultValue(Object o) {this.defaultValue = o;}
 		void setCsvIndex(int pos) {this.csvIndex = pos;}
 		void setFormat(String format) {this.format = format;}
+
+		private String strip(String str, String chars){
+			int l, r;
+			
+			for (l=0; l<str.length() && chars.indexOf(str.charAt(l))!=-1; ++l);
+			for (r=str.length(); r>l && chars.indexOf(str.charAt(r-1))!=-1; --r);
+			
+			return str.substring(l, r);
+		}
 		
 		public String getValue(String tokens[]){
 			String value = null;
 			
 			if (csvIndex == 0)
 				value = defaultValue.toString();
-			if (tokens.length>=csvIndex)
+			else if (tokens.length>=csvIndex)
 				value = tokens[csvIndex-1];
-			if (defaultValue!=null)
+			else if (defaultValue!=null)
 				value = defaultValue.toString();
 			if (value!=null){
+				value = strip(value, " \t\n\"'");
 				if (format!=null)
 					value = String.format(format, value);
 				return value;
@@ -71,34 +83,56 @@ public class Importer {
 	
 	public void setArgument(int index, Object defaultValue, int csvIndex, String format){
 		if (order.size()>=index){
-			order.get(index).setDefaultValue(defaultValue);
-			order.get(index).setCsvIndex(csvIndex);
-			order.get(index).setFormat(format);
+			order.get(index-1).setDefaultValue(defaultValue);
+			order.get(index-1).setCsvIndex(csvIndex);
+			order.get(index-1).setFormat(format);
 		}
 	}
 	
 	public void importFile(String path){
-		Connection con = DB.getConnection();
+		if (tableName==null || path==null)
+			return;
+		
+		Connection con = null;
+		DatabaseMetaData metadata = null;
+		ResultSet columns = null;
 		Statement stmt = null;
 		Scanner scanner = null;
 		String query = "insert into %s values(%s)";
 		try {
+			con = DB.getConnection();
+			metadata = con.getMetaData();
+			columns = metadata.getColumns(null, null, tableName, null);
+			for (int i=0; i<order.size(); ++i){
+				columns.next();
+				if (columns.getString("TYPE_NAME").equals("VARCHAR2")){
+					order.get(i).setFormat("'%s'");
+				}
+			}
+			
+			
 			scanner = new Scanner(new FileReader(path));
 			stmt = con.createStatement();
 			while (scanner.hasNextLine()){
 				String line = scanner.nextLine();
 				String tokens[] = line.split(",");
 				StringBuilder values = new StringBuilder();
-				for (Parameter p : order){
-					if (values.length()>0){
-						values.append(", " + p.getValue(tokens));
-					}
-					else{
-						values.append(p.getValue(tokens));
+				if (order==null){
+					values.append(line);
+				}
+				else{
+					for (Parameter p : order){
+						if (values.length()>0){
+							values.append(", " + p.getValue(tokens));
+						}
+						else{
+							values.append(p.getValue(tokens));
+						}
 					}
 				}
-				stmt.executeQuery(String.format(query, tableName, values));
+				stmt.execute(String.format(query, tableName, values));
 			}
+			stmt.execute("commit");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
