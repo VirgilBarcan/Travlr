7ea64f7d94678ff4082940715e4d6826c598dc5d
berendeanicolae <berendeanicolae@gmail.com>
2015-06-01 12:30:07 +0300

    import flights into db

--- app/service/Importer.java
+++ app/service/Importer.java
@@ -2,10 +2,15 @@ package service;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.JDBCType;
 import java.sql.ResultSet;
+import java.sql.SQLData;
 import java.sql.SQLException;
+import java.sql.SQLType;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -37,36 +42,36 @@ public class Importer {
 			return str.substring(l, r);
 		}
 		
-		public String getValue(String tokens[]){
+		public String getValue(Object tokens[]){
 			String value = null;
 			
 			if (csvIndex == 0)
 				value = defaultValue.toString();
 			else if (tokens.length>=csvIndex)
-				value = tokens[csvIndex-1];
+				value = tokens[csvIndex-1].toString();
 			else if (defaultValue!=null)
 				value = defaultValue.toString();
 			if (value!=null){
 				value = strip(value, " \t\n\"'");
 				if (format!=null)
-					value = String.format(format, value);
+					value = String.format(format, value.toString());
 				return value;
 			}
 			return "NULL";
 		}
 	}
 	
-	private String tableName;
+	private String procName;
 	private ArrayList<Parameter> order;
 	
 	public Importer(){
-		tableName = null;
+		procName = null;
 		order = null;
 	}
 	
-	public void setTableName(String tableName){
-		if (DatabaseLayer.isTable(tableName)){
-			this.tableName = tableName;
+	public void setProcName(String procName){
+		if (DatabaseLayer.isTable(procName)){
+			this.procName = procName;
 		}
 	}
 	
@@ -88,51 +93,89 @@ public class Importer {
 			order.get(index-1).setFormat(format);
 		}
 	}
+
+	public Object[] split(String str, char sep){
+		boolean in = false;
+		
+		ArrayList<StringBuilder> tokens = new ArrayList<StringBuilder>();
+		for (int i=0; i<str.length(); ++i){
+			if (str.charAt(i)=='"'){
+				in = !in;
+				continue;
+			}
+			if (in){
+				tokens.get(tokens.size()-1).append(str.charAt(i));
+			}
+			else{
+				if (str.charAt(i)==sep){
+					tokens.add(new StringBuilder());
+					continue;
+				}
+				if (tokens.isEmpty()){
+					tokens.add(new StringBuilder());
+				}
+				tokens.get(tokens.size()-1).append(str.charAt(i));
+			}
+		}
+		
+		ArrayList<String> stringTokens = new ArrayList<String>();
+		for (int i=0; i<tokens.size(); ++i){
+			stringTokens.add(tokens.get(i).toString());
+		}
+		return stringTokens.toArray();
+	}
 	
 	public void importFile(String path){
-		if (tableName==null || path==null)
+		if (procName==null || path==null)
 			return;
 		
 		Connection con = null;
-		DatabaseMetaData metadata = null;
-		ResultSet columns = null;
-		Statement stmt = null;
-		Scanner scanner = null;
-		String query = "insert into %s values(%s)";
-		try {
+		ArrayList<SQLType> procedureTypes = new ArrayList<SQLType>();
+		String query = "call %s (%s)";
+		try{
 			con = DB.getConnection();
-			metadata = con.getMetaData();
-			columns = metadata.getColumns(null, null, tableName, null);
-			for (int i=0; i<order.size(); ++i){
-				columns.next();
+			DatabaseMetaData metadata = con.getMetaData();
+			ResultSet columns = metadata.getProcedureColumns(null, null, procName.substring(procName.indexOf('.')+1), null);
+			StringBuilder queryValues = new StringBuilder();
+			for (int i=0; columns.next(); ++i){
 				if (columns.getString("TYPE_NAME").equals("VARCHAR2")){
-					order.get(i).setFormat("'%s'");
+					//order.get(i).setFormat("'%s'");
 				}
-			}
-			
-			
-			scanner = new Scanner(new FileReader(path));
-			stmt = con.createStatement();
-			while (scanner.hasNextLine()){
-				String line = scanner.nextLine();
-				String tokens[] = line.split(",");
-				StringBuilder values = new StringBuilder();
-				if (order==null){
-					values.append(line);
+				procedureTypes.add(JDBCType.values()[columns.getInt("DATA_TYPE")]);
+				if (queryValues.length()>0){
+					queryValues.append(", ?");
 				}
 				else{
-					for (Parameter p : order){
-						if (values.length()>0){
-							values.append(", " + p.getValue(tokens));
-						}
-						else{
-							values.append(p.getValue(tokens));
-						}
+					queryValues.append("?");
+				}
+			}
+			//System.out.println(procedureTypes);
+			query = String.format(query, procName, queryValues);
+		}
+		catch (SQLException e){
+			e.printStackTrace();
+			return;
+		}
+		
+		try {
+			Scanner scanner = new Scanner(new FileReader(path));
+			CallableStatement stmt = con.prepareCall(query);
+			while (scanner.hasNextLine()){
+				String line = scanner.nextLine();
+				Object tokens[] = split(line, ',');
+				System.out.println(line);
+				if (order==null){
+					//values.append(line);
+				}
+				else{
+					for (int i=0; i<order.size(); ++i){
+						stmt.setObject(i+1, order.get(i).getValue(tokens));
 					}
 				}
-				stmt.execute(String.format(query, tableName, values));
+				stmt.execute();
 			}
 			stmt.execute("commit");
+			scanner.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
