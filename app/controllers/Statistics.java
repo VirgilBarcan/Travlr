707a815ced274c3c48a95778ebde0d1ed3ca7baa
FIIAurelian <FIIAurelian@users.noreply.github.com>
2015-06-03 15:52:13 +0300

    Update Statistics.java

--- app/controllers/Statistics.java
+++ app/controllers/Statistics.java
@@ -9,9 +9,18 @@ import views.html.*;
 import java.util.HashMap;
 
 public class Statistics extends Controller{
-	public static Result index() {
+	public static Result index(String nr) {
         UserInfo userInfo = UserData.getUserInfoFromDB();
-        HashMap<String, Double> harta = DatabaseLayer.topCompanies("5");
-        return ok(statistics.render(harta, userInfo));
+        HashMap<String, Double> harta = DatabaseLayer.topCompanies(nr);
+        String message, al;
+     	if(harta.size() < Integer.parseInt(nr)) {
+     		message = "We do not have enough data! We have rating for " + harta.size() + " companies out of " + nr;
+     		al = "alert-warning";
+     	}
+     	else {
+     		message = "We have enough data for this query!";
+     		al = "alert-info";
+     	}
+        return ok(statistics.render(harta, message, al, userInfo));
 	}
 }
